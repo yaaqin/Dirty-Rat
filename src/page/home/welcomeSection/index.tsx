cfc285babe7c4c0e1e6b238f67951624f31f6f09
developerchaincode <yaqin.me@gmail.com>
2025-02-23 23:36:32 +0700

    update kemiirngan

--- src/page/home/welcomeSection/index.tsx
+++ src/page/home/welcomeSection/index.tsx
@@ -5,8 +5,8 @@ export default function WelcomeSection() {
   return (
     <section
       className="bg-primaryColor py-[100px] relative mt-0 md:-mt-24">
-      <section className='absolute w-full h-1/6 -top-1 bg-black z-40' style={{ clipPath: 'polygon(0% 0%, 100% 0%, 0% 100%)' }}></section>
-      <section className='absolute w-full h-1/6 bg-primaryColor z-40 -bottom-1' style={{ clipPath: 'polygon(100% 0%, 100% 100%, 0% 100%)' }}></section>
+      <section className='absolute w-full -top-1 bg-black z-40' style={{height: '14%', clipPath: 'polygon(0% 0%, 100% 0%, 0% 100%)' }}></section>
+      <section className='absolute w-full bg-primaryColor z-40 -bottom-1' style={{height: '14%', clipPath: 'polygon(100% 0%, 100% 100%, 0% 100%)' }}></section>
       {/* <section className='absolute w-full h-24 bottom-0 bg-primaryColor z-40'></section> */}
       <video
         className="w-full object-cover"
