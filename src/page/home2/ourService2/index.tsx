e4c1c55d6480b6fea7a88a6fe1eb62043c31d98e
developerchaincode <yaqin.me@gmail.com>
2025-03-09 22:59:04 +0700

    update deskription

--- src/page/home2/ourService2/index.tsx
+++ src/page/home2/ourService2/index.tsx
@@ -8,8 +8,7 @@ export default function OurService2() {
             <div className="absolute md:mb-0 text-end text-white ml-auto pt-36 h-screen z-20 flex flex-col px-4 md:px-16 items-end w-full">
                 <h2 className="text-3xl md:text-4xl font-bold mb-4">OUR SERVICES</h2>
                 <p className="text-base md:text-lg mb-6 w-full md:w-2/3 text-right text-shadow">
-                We build for your platform VR, Mobile, TV, Console, or PC. We work the way you work, fitting into your team’s habits, tools, and timelines to keep projects flowing smoothly.
-                </p>
+                We build for your platform VR, Mobile, TV, Console, or PC. We work the way you work, fitting into your team’s habits, tools, and timelines, Like a rat discovering every path, Dirty Rat navigates the maze of 3D artistry, delivering creative solutions and unmatched expertise at every turn.</p>
                 <ul className="space-y-2 text-end w-full md:w-2/3 ml-auto capitalize" style={{ listStylePosition: 'inside', listStyleType: 'none' }}>
                     <li className="flex justify-end text-base md:text-lg">Game Ready & VFX character & creature modeling<span className="ml-2">•</span></li>
                     <li className="flex justify-end text-base md:text-lg">Hair card & XGEN grooming<span className="ml-2">•</span></li>
