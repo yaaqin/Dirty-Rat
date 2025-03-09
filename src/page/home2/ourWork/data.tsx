5afab7da2c3e3543155616088f34e8a4181861e6
developerchaincode <yaqin.me@gmail.com>
2025-03-09 09:01:28 +0700

    relocate to public

--- src/page/home2/ourWork/data.tsx
+++ src/page/home2/ourWork/data.tsx
@@ -1,10 +1,10 @@
-import img1 from '../../../assets/img/ourWork/christopher-goh-witchbitch (1).jpg'
-import img7 from '../../../assets/img/ourWork/2 (1).jpg'
-import img10 from '../../../assets/img/ourWork/3.jpg'
-import img17 from '../../../assets/img/ourWork/4.jpg'
-import img24 from '../../../assets/img/ourWork/5.jpg'
-import img32 from '../../../assets/img/ourWork/Emily_Hair_Preview_05 (1).png'
-import img33 from '../../../assets/img/ourWork/7.jpg'
-import img34 from '../../../assets/img/ourWork/8.jpg'
+import gambar1 from '../../../../public/assets/OurWork/christopher-goh-witchbitch (1).jpg'
+import img7 from '../../../../public/assets/OurWork/2 (1).jpg'
+import img10 from '../../../../public/assets/OurWork/ketiga.jpg'
+import img17 from '../../../../public/assets/OurWork/keempat.jpg'
+import img24 from '../../../../public/assets/OurWork/kelima.jpg'
+import img32 from '../../../../public/assets/OurWork/Emily_Hair_Preview_05 (1).png'
+import img33 from '../../../../public/assets/OurWork/ketuju.jpg'
+import img34 from '../../../../public/assets/OurWork/kelapan.jpg'
  
-export const OurPortfolio = [img1, img7, img10, img17, img24,img32, img33, img34]
+export const OurPortfolio = [gambar1, img7, img10, img17, img24,img32, img33, img34]
