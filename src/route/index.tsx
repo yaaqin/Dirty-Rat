dd9b0c17f4fa5be651149a0b4aacfdd4ea7d4f1c
developerchaincode <yaqin.me@gmail.com>
2025-03-14 08:47:48 +0700

    route setting

--- src/route/index.tsx
+++ src/route/index.tsx
@@ -4,10 +4,6 @@ import Contact from "../page/contact";
 import Home2 from "../page/home2";
 
 export const AppRoutes = createBrowserRouter([
-    // {
-    //     path: '/home2',
-    //     element: <Home/>
-    // },
     {
         path: '/',
         element: <Home2/>
--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/',  // Atur base URL sesuai dengan path aplikasi kamu
 })
