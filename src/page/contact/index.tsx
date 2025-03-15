7bb3d713d66a6556aec1bab300951b0cd88e3d71
developerchaincode <yaqin.me@gmail.com>
2025-03-15 14:11:47 +0700

    useeffect

--- src/page/contact/index.tsx
+++ src/page/contact/index.tsx
@@ -1,7 +1,11 @@
+import { useEffect } from 'react';
 import Navbar from '../home/navbar'
 import MainComponent from './section'
 
 export default function Contact() {
+    useEffect(() => {
+        window.scrollTo({ top: 0, behavior: "smooth" });
+    }, []);
     return (
         <>
             <section className="fixed top-0 left-0 w-full z-20">
