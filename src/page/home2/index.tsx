7efd46a401b542ab1fdd5340de9b2a01d754a234
developerchaincode <yaqin.me@gmail.com>
2025-03-16 23:09:48 +0700

    useEffect

--- src/page/home2/index.tsx
+++ src/page/home2/index.tsx
@@ -4,14 +4,15 @@ import OwnerList from '../home/ownerList'
 import Footer from '../home/footer'
 import OurService2 from './ourService2'
 import OurWork from './ourWork'
+import { useLocation } from 'react-router-dom';
 import WelcomeSection from '../home/welcomeSection'
 import StudioSection from '../home/studioSection'
 import { useEffect } from 'react'
 
 export default function Home2() {
   useEffect(() => {
-    window.scrollTo({ top: 0, behavior: "smooth" });
-  }, []);
+  window.scrollTo({ top: 0, behavior: "smooth" });
+}, [useLocation().pathname]);
   return (
     <>
       <section className="fixed top-0 left-0 w-full z-50">
